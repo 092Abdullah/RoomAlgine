14d205f41f60f0234d50e4dd9fd3c43ddb0b1f8b
Mian Abdullah Maqbool <maqboolmianabdullah@gmail.com>
2025-08-10 14:13:25 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Paintbrush classN

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,7 +5,7 @@ export function LogoIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <div className="flex items-center gap-2">
       <div className="relative h-8 w-8">
-        <Paintbrush {...props} className="h-full w-full text-primary" />
+        <Home {...props} className="h-full w-full text-primary" />
         <Sparkles {...props} className="absolute -top-1 -right-1 h-4 w-4 text-purple-400" />
       </div>
       <span className="text-xl font-bold tracking-tight">RoomAIgine</span>
