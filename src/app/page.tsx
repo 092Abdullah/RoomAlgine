b4e5cbc6a10b4c3cbb3fba8355a19890407cc4b0
Mian Abdullah Maqbool <maqboolmianabdullah@gmail.com>
2025-08-28 20:08:19 +0000

    orr aik or change krna ha k,
    agr user logged in state mn ho,tou woh hrr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 
 import LandingPage from "@/components/landing-page";
 import { createSupabaseServerClient } from "@/lib/supabase/server";
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const supabase = await createSupabaseServerClient();
   const { data: { user } } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect('/dashboard');
+  }
 
   return <LandingPage user={user} />;
 }
