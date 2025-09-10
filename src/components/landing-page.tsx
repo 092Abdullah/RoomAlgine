5693e77113c51a9a7fcdc45950097892ec521890
Mian Abdullah Maqbool <maqboolmianabdullah@gmail.com>
2025-09-10 16:59:17 +0000

    can u do setting like,the video always show in 1080p hd,

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -169,7 +169,7 @@ const LandingPage = ({ user }: { user: User | null }) => {
              <div className="relative aspect-video w-full max-w-6xl mx-auto rounded-xl overflow-hidden">
                 <iframe
                     className="absolute top-0 left-0 w-full h-full"
-                    src="https://www.youtube.com/embed/kPkFSeLGCXQ?playlist=kPkFSeLGCXQ&autoplay=1&loop=1&mute=1&controls=0&showinfo=0&modestbranding=1"
+                    src="https://www.youtube.com/embed/kPkFSeLGCXQ?playlist=kPkFSeLGCXQ&autoplay=1&loop=1&mute=1&controls=0&showinfo=0&modestbranding=1&vq=hd1080"
                     title="YouTube video player"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
