a5a6d259f15404d43ccd9f2b7e48fb0312e7c52b
Mian Abdullah Maqbool <maqboolmianabdullah@gmail.com>
2025-11-16 19:23:28 +0000

    [GoogleGenerativeAI Error]: Error fetching from https://generativelangua

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,5 +4,5 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [googleAI()],
   // This model is suitable for vision and text tasks.
-  model: 'googleai/gemini-1.5-pro',
+  model: 'googleai/gemini-1.5-pro-latest',
 });
