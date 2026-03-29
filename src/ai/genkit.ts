664564e3a66cb032931d7aef19628d0529568b74
Abdullah Maqbool <maqboolmianabdullah@gmail.com>
2026-03-30 01:14:37 +0500

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -14,5 +14,5 @@ if (typeof fetch !== 'function') {
 export const ai = genkit({
   plugins: [googleAI()],
   // This model is suitable for vision and text tasks and is known to be available.
-  model: 'gemini-3.1-flash-image-preview,
+  model: 'gemini-3.1-flash-image-preview',
 });
